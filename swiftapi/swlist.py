df30e39270c480c5ccaab7d6ed9140fe2a263abe
Patrick Galbraith <patg@hp.com>
2012-02-22 18:31:28 +0000

    Added work in progress on swift "API". Very much work in progress!

---
+++ swiftapi/swlist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import swift
+from os import environ, listdir, makedirs, utime
+import socket
+
+try:
+    from eventlet.green.httplib import HTTPException, HTTPSConnection
+except ImportError:
+    from httplib import HTTPException, HTTPSConnection
+
+opts = {    'auth' : environ.get('ST_AUTH'),
+            'user' : environ.get('ST_USER'),
+            'key' : environ.get('ST_KEY'),
+            'snet' : False,
+            'prefix' : '',
+            'auth_version' : '1.0'}
+
+try:
+    items = swift.st_list(opts, ['mysql-backup'])
+
+except (swift.ClientException, HTTPException, socket.error), err:
+    error_queue.put(str(err))
+
+for item in items:
+    print item.get('name')
