3ec3aa30d276d5ef6efedb45d3532776a290afa5
Kevin Mansel <kevin.mansel@hp.com>
2012-03-05 10:05:56 -0800

    Updated test files to include HP copyright

--- reddwarf/tests/hpcs/swift_test.py
+++ reddwarf/tests/hpcs/swift_test.py
@@ -1,4 +1,5 @@
 #    Copyright 2011 OpenStack LLC
+#    Copyright 2012 HP Software, LLC
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,6 +12,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+
 
 import os
 import unittest
