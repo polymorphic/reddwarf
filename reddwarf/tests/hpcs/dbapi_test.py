2f880049ae12f857a740d328a413697d15215454
Josh Dorothy <josh.dorothy@hp.com>
2012-03-06 16:58:35 -0800

    More API test changes

--- reddwarf/tests/hpcs/dbapi_test.py
+++ reddwarf/tests/hpcs/dbapi_test.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import logging
 import unittest
 import json
 import httplib2
@@ -21,17 +22,62 @@ AUTH_TOKEN = "abc:123"
 AUTH_HEADER = {'X-Auth-Token': AUTH_TOKEN, 'content-type': 'application/json', 'Accept': 'application/json'}
 created_instance = False
 
+logging.basicConfig()
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
 class DBFunctionalTests(unittest.TestCase):
     
     def test_instance_api(self):
         """Comprehensive instance api test using an instance lifecycle."""
         
         """1) Test creating a db instance."""
-        """Current do nothing!"""
-        self.instance_id = "915d5cd9-6c9f-4a49-aa2a-3de8603bf7f1"
-    
+        LOG.debug("* Creating db instance")
+        body = r"""
+        {"instance": {
+            "name": "dbapi_test",
+            "flavorRef": "102",
+            "port": "3306",
+            "dbtype": {
+                "name": "mysql",
+                "version": "5.1.2"
+            },
+            "databases": [
+                {
+                    "name": "testdb",
+                    "character_set": "utf8",
+                    "collate": "utf8_general_ci"
+                },
+                {
+                    "name": "abcdefg"
+                }
+            ],
+            "volume":
+                {
+                    "size": "2"
+                }
+            }
+        }"""
+        
+        req = httplib2.Http(".cache")
+        resp, content = req.request(API_URL + "instances", "POST", body, AUTH_HEADER)
+        content = json.loads(content)
+        LOG.debug(resp)
+        LOG.debug(content)
+        self.instance_id = content['instance']['id']
+        LOG.debug("Instance ID: %s" % self.instance_id)
+        
+
+        """Assert 1) that the request was accepted and 2) that the response
+           is in the expected format."""
+        self.assertEqual(resp.status, 200)
+        self.assertTrue(content.has_key('instance'))
+        #self.instance_id = "e48fe6b6-77d5-4619-9ed7-8f39231bded7"
+       
     
         """2) Test listing all db instances."""
+        LOG.debug("* Listing all db instances")
         req = httplib2.Http(".cache")
         resp, content = req.request(API_URL + "instances", "GET", "", AUTH_HEADER)
         content = json.loads(content)
@@ -46,6 +92,7 @@ class DBFunctionalTests(unittest.TestCase):
     
     
         """3) Test getting a specific db instance."""
+        LOG.debug("* Getting instance %s" % self.instance_id)
         req = httplib2.Http(".cache")
         resp, content = req.request(API_URL + "instances/" + self.instance_id, "GET", "", AUTH_HEADER)
         content = json.loads(content)
@@ -59,6 +106,7 @@ class DBFunctionalTests(unittest.TestCase):
     
     
 #        """4) Test resetting the password on a db instance."""
+#        LOG.debug("* Resetting password on instance %s" % self.instance_id)
 #        req = httplib2.Http(".cache")
 #        resp, content = req.request(API_URL + "instances/" + self.instance_id + "/resetpassword", "POST", "", AUTH_HEADER)
 #        #content = json.loads(content)
@@ -70,27 +118,36 @@ class DBFunctionalTests(unittest.TestCase):
 #        #self.assertEqual(self.instance_id, content['instance']['id'])   
     
     
-        """5) Test restarting a db instance."""
-        req = httplib2.Http(".cache")
-        resp, content = req.request(API_URL + "instances/" + self.instance_id + "/restart", "POST", "", AUTH_HEADER)
-        #content = json.loads(content)
-        
-        print resp
-        print content
-        """Assert 1) that the request was accepted."""
-        self.assertEqual(resp.status, 202)
-        #self.assertEqual(self.instance_id, content['instance']['id'])
+#        """5) Test restarting a db instance."""
+#        LOG.debug("* Restarting instance %s" % self.instance_id)
+#        LOG.debug("  - Sending %s command: %s" % ("POST", API_URL + "instances/" + self.instance_id + "/restart"))
+#        req = httplib2.Http(".cache")
+#        resp, content = req.request(API_URL + "instances/" + self.instance_id + "/restart", "POST", "", AUTH_HEADER)
+#        #content = json.loads(content)
+#        
+#        print resp
+#        print content
+#        """Assert 1) that the request was accepted."""
+#        self.assertEqual(resp.status, 202)
+#        #self.assertEqual(self.instance_id, content['instance']['id'])
     
     
         """6) Test deleting a db instance."""
+        LOG.debug("* Deleting instance %s" % self.instance_id)
         req = httplib2.Http(".cache")
         resp, content = req.request(API_URL + "instances/" + self.instance_id, "DELETE", "", AUTH_HEADER)
-        #content = json.loads(content)
         
         print resp
         print content
-        """Assert 1) that the request was accepted."""
+        """Assert 1) that the request was accepted and 2) that the instance no
+           longer exists."""
         self.assertEqual(resp.status, 202)
+        
+        resp, content = req.request(API_URL + "instances", "GET", "", AUTH_HEADER)
+        content = json.loads(content)
+        
+        for each in content['instances']:
+            self.assertFalse(each['id'] == self.instance_id)
     
     def test_dbsnapshot_list(self):
         """Test listing all db snapshots."""
