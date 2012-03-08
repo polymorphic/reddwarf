6c57051c5c9ee85a57ba45d6c23e8a43b7f43769
Patrick Galbraith <patg@hp.com>
2012-03-07 16:27:32 -0800

    Added image building scripts

--- bin/initscript.sh
+++ bin/initscript.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-# don't ask me no questions and I won't tell you no lies 
-export DEBIAN_FRONTEND=noninteractive
+# stop mysql
+service mysql stop
 
-# import PGP keys
-gpg --keyserver  hkp://keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-gpg -a --export CD2EFD2A | apt-key add -
-
-# set up the percona repo
-echo "deb http://repo.percona.com/apt lucid main
-deb-src http://repo.percona.com/apt lucid main" >/etc/apt/sources.list.d/percona.list
-
-apt-get update
-apt-get --force-yes --yes install percona-server-common-5.5  percona-server-server-5.5 percona-server-test-5.5  percona-server-client-5.5 libmysqlclient18  libmysqlclient-dev xtrabackup
-
-# change root password
-/usr/bin/mysqladmin -u root password hpcs
-
-# now shut down
-/etc/init.d/mysql stop
-
-# we will be moving this to a new mount point
+# back up data directory
 mv /var/lib/mysql /var/lib/mysql.bak
-
-# copy stock my.cnf
-echo "[mysqld]
-user=mysql
-
-datadir=/var/lib/mysql
-
-innodb_buffer_pool_size=2G
-innodb_log_file_size=100M
-innodb_file_per_table 
-" > /etc/mysql/my.cnf
-
-# remove innodb log file since log file size is changed
-rm /var/lib/mysql.bak/ib_logfile*
 
 # partition, set as LVM
 # this WILL be dynamic/generated depending on what hosts the user wants to allow, for now we open wide
@@ -50,9 +19,6 @@ t
 w
 EOF
 
-# install lvm
-apt-get -y install lvm2
-
 # creat physical volume
 pvcreate /dev/vdb1
 
@@ -60,16 +26,16 @@ pvcreate /dev/vdb1
 vgcreate data /dev/vdb1
 
 # create logical volume - this needs to be changed to account for flavor (size)
-lvcreate --size 100G --name mysql-data data
-
-# install xfs utils
-apt-get -y install xfsprogs xfsdump
+lvcreate --size 20G --name mysql-data data
 
 # format
 mkfs.xfs /dev/data/mysql-data
 
 # create mount point
 mkdir /var/lib/mysql
+
+# copy data from backup
+cp -a /var/lib/mysql.bak /var/lib/mysql
 
 # change ownership
 chown mysql:mysql /var/lib/mysql
@@ -80,33 +46,20 @@ echo -e "\n/dev/data/mysql-data\t/var/lib/mysql\txfs\tdefaults\t0\t0\n" >> /etc/
 # mount
 mount -a 
  
-# copy data
-cp -a /var/lib/mysql.bak/* /var/lib/mysql/
-
-# ensure ownership
-chown -R mysql:mysql /var/lib/mysql
-
 # start
 /etc/init.d/mysql start
 
-# this WILL be dynamic
-/usr/bin/mysql -u root -phpcs -e "grant all privileges on *.* to 'root'@'%' identified by 'hpcs' with grant option;"
-
-apt-get install -qqy git
-gid="$(getent passwd mysql | cut -f4 -d:)"
-useradd -m -g $gid nova
 cd /home/nova
-sudo mkdir lock
-sudo mkdir logs
 sudo git clone https://github.com/hpcloud/reddwarf.git
+cp /home/nova/reddwarf/smartagent/startup/disk_prep /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/smartagent /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/mysql.conf /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/sudoers /etc
+update-rc.d disk_prep defaults 85
+update-rc.d smart_agent defaults 90
 
-# Create a .my.cnf for the Agent
-echo "[client]
-user=root
-password=hpcs
-" > /home/nova/.my.cnf
-
-ln -s /home/nova/.my.cnf /root/.my.cnf
+# make sure nova owns
+chown -R nova:mysql /home/nova
 
 ######## TEMPORARY agent.config file ########
 #echo "[messaging]
@@ -117,11 +70,6 @@ ln -s /home/nova/.my.cnf /root/.my.cnf
 #" > /home/nova/agent.config
 ##########################
 
-apt-get install -qqy python-pip python-dev
-pip install pika
-pip install mysql-python
-pip install swift
-pip install --upgrade python-daemon
 cd reddwarf
 ln -s /home/nova/reddwarf/smartagent/smartagent_launcher.py /etc/init.d/smartagent
 sudo /etc/init.d/smartagent start
--- smartagent/startup/dbaas-image-full.sh
+++ smartagent/startup/dbaas-image-full.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# this version of the script does everything up to what the
-# cloud init should do
+# this does *everything*, builds a fully-stocked image
 
 # don't ask me no questions and I won't tell you no lies 
 export DEBIAN_FRONTEND=noninteractive
@@ -32,12 +31,16 @@ sed -i '$d' /etc/fstab
 /usr/bin/mysqladmin -u root password hpcs 
 # create agent DB account
 # TODO: get privileges set up so agent can't read user, vice versa
-/usr/bin/mysql -u root -p hpcs -c "grant all privileges on *.* to 'os_admin'@'localhost' identified by 'hpcs' with grant option;"
+/usr/bin/mysql -c "grant all privileges on *.* to 'os_admin'@'localhost' identified by 'hpcs' with grant option;"
 # create user account. TODO: this will be passed via the API
-/usr/bin/mysql -u root -p hpcs -c "grant all privileges on *.* to 'dbas'@'localhost' identified by 'hpcs' with grant option;"
+/usr/bin/mysql -c "grant all privileges on *.* to 'dbas'@'localhost' identified by 'hpcs' with grant option;"
 
 # now shut down
 /etc/init.d/mysql stop
+
+mv /var/lib/mysql /var/lib/mysql.bak
+
+umount /mnt
 
 # remove innodb log file since log file size is changed
 rm /var/lib/mysql.bak/ib_logfile*
@@ -54,8 +57,41 @@ innodb_log_file_size=100M
 innodb_file_per_table 
 " > /etc/mysql/my.cnf
 
+
+fdisk /dev/vdb <<EOF
+d
+n
+p
+1
+
+
+t
+8e
+w
+EOF
+
+pvcreate /dev/vdb1
+vgcreate data /dev/vdb1
+lvcreate --size 20G --name mysql-data data
+mkfs.xfs /dev/data/mysql-data
+
+mkdir /var/lib/mysql
+
+chown mysql:mysql /var/lib/mysql
+
+echo -e "\n/dev/data/mysql-data\t/var/lib/mysql\txfs\tdefaults\t0\t0\n" >> /etc/fstab
+
+mount -a 
+
+cp -a /var/lib/mysql.bak/* /var/lib/mysql/
+
+chown -R mysql:mysql /var/lib/mysql
+
 # start
 /etc/init.d/mysql start
+
+# this WILL be dynamic
+/usr/bin/mysql -u root -phpcs -e "grant all privileges on mysql.* to 'os_admin'@'localhost' identified by 'hpcs' with grant option;" 
 
 useradd -d /home/nova -g mysql -m -s /bin/bash -p n0va nova
 mkdir /home/nova/logs
@@ -64,3 +100,12 @@ mkdir /home/nova/backup_logs
 chown nova -R /home/nova/
 mkdir /var/lib/mysql-backup
 chown nova:mysql /var/lib/mysql-backup
+
+cd /home/nova
+git clone git clone git@github.com:hpcloud/reddwarf.git
+cp /home/nova/reddwarf/smartagent/startup/disk_prep /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/smartagent /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/mysql.conf /etc/init.d
+cp /home/nova/reddwarf/smartagent/startup/sudoers /etc
+update-rc.d disk_prep defaults 85
+update-rc.d smart_agent defaults 90
